02f537a07bbdcf86152bbba4749270ea8f2385bb
Russ Moore <russcmoore314@gmail.com>
2023-12-18 16:32:05 -0500

    poller pulling automobiles from the inventory microservice and creating VOs

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = (
+    ("CREATED", "created"),
+    ("CANCELLED", "cancelled"),
+    ("FINISHED", "finished"),
+)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17)
+    sold = models.BooleanField()
 
 
-class Technician(models.Model)
+class Technician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_id = models.CharField(max_length=51)
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default="CREATED")
+    vin = models.CharField(max_length=17)
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name='appointmens',
+        on_delete=models.CASCADE
+    )
